92aeefac56642d1228088259eb9b63bcebe78572
draquel <draquel@webjynx.com>
2020-12-28 13:11:20 -0500

    added discord command
    added command for sharing discord server info. and updated readme

--- PathfinderTools.lua
+++ PathfinderTools.lua
@@ -1,5 +1,11 @@
 PathfinderTools = { }
 
+local defaultRecruitmentMsg = "Hello %p, would you be interested in joining a leveling guild? We're a light hearted group looking for more people interested in quest and dungeon parties."
+local defaultPublicWelcomeMsg = "Welcome %p"
+local defaultOrientationMsg = "So the guild is currently in open recruitment. This means that everyone can recruit and that you should feel free to /ginvite people you meet in your travels who could benifit from guild membership. We try to organize dungeon runs in the evenings and weekends so watch out for those. Also we have a shiny new Discord server, link in the MOTD, which you can check out if you are interested."
+
+--discordInfo = "Discord Server: [https://discord.gg/vMUmvY9Fpy] - Please set your nickname to match your character's name."
+
 function PathfinderTools:getTargetPlayer()
     if UnitName("Target") ~= nil and UnitIsPlayer("Target") then
         name = UnitName("Target")
@@ -10,7 +16,7 @@ function PathfinderTools:getTargetPlayer()
     end
 end
 
-function PathfinderTools:playerSelection(name)
+function PathfinderTools:PlayerSelection(name)
     if name == nil or name == "" then
         name = PathfinderTools:getTargetPlayer()
     end
@@ -18,7 +24,7 @@ function PathfinderTools:playerSelection(name)
 end
 
 function PathfinderTools:Recruit(name)
-    player = PathfinderTools:playerSelection(name)
+    player = PathfinderTools:PlayerSelection(name)
     
     if UnitIsUnit(player,"player") then
         print("PF Error: You can't recruit yourself....")
@@ -28,7 +34,7 @@ function PathfinderTools:Recruit(name)
 end
 
 function PathfinderTools:Welcome(name)
-    player = PathfinderTools:playerSelection(name)
+    player = PathfinderTools:PlayerSelection(name)
 
     if UnitIsUnit(player,"player") then
         print("PF Error: You can't welcome yourself....")
@@ -39,19 +45,39 @@ function PathfinderTools:Welcome(name)
     end
 end
 
+function PathfinderTools:ShareDiscord(with)
+    local discordInfo = "Pathfinders Discord Server: https://discord.gg/vMUmvY9Fpy - Please set your server nickname to match your character's name."
+    if with == "raid" or with == "party" or with == "guild" then
+        SendChatMessage(discordInfo, with:upper(), "Common", nil)
+    else
+        player = PathfinderTools:PlayerSelection(with)
+        if player ~= nil then
+            SendChatMessage(discordInfo, "WHISPER", "Common", player)
+        end
+    end
+end
+
 local frame = CreateFrame("FRAME")
 frame:RegisterEvent("ADDON_LOADED")
+
+function frame:OnEvent(event,arg)
+    if event == "ADDON_LOADED" and arg == "PathfinderTools" then
+        
+    end
+end
 
 SLASH_PF1 = '/pf'
 function SlashCmdList.PF(args, editbox)
     local _, _, cmd, arg = string.find(args, "%s?(%w+)%s?(.*)")
 
-    --print(cmd.." : "..args)
+    print(cmd.." : "..arg)
 
     if cmd == 'recruit' then
         PathfinderTools:Recruit(arg)
     elseif cmd == 'welcome' then
         PathfinderTools:Welcome(arg)
+    elseif cmd == "discord" then
+        PathfinderTools:ShareDiscord(arg)
     else
         print("PF Error: Invalid command.")
     end
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PathfinderTools
- 
+
+    Simple chat command tools to make recruiting easier and streamlined
+
+## Commands
+
+### Recruit
+
+    /pf recruit [name] - Whisper recruitment blurb to the player [name] or your current target if name is omitted
+
+### Welcome
+
+    /pf welcome [name] - Sends obiligitory "Welcome [name]" to guild chat and whispers intro information about guild to [name]. Defaults to current target if [name] is omitted.
+
+### Discord
+
+    /pf discord [destination] - Sends discord server link to [destination]. Accepted destinations include "raid", "party", "guild" or a player name. If the destination is omitted the current target will be used if it is a player.
+
+## To-Do
+
+    * Create options to customize recruitment and welcome text
+    * Add functionality to promote new members to Recruit
