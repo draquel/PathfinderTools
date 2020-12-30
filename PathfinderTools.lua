bf84d210d7cbab1605e96274cfdb2a6d7e1605a2
draquel <draquel@webjynx.com>
2020-12-30 12:01:13 -0500

    Stats
    Added stats command and persistence as precursor to creating config options.
    
    Updated readme to match and include new goals

--- PathfinderTools.lua
+++ PathfinderTools.lua
@@ -1,9 +1,18 @@
 PathfinderTools = { }
 
-local defaultRecruitmentMsg = "Hello {p}, would you be interested in joining a leveling guild? We're a light hearted group looking for more people interested in quest and dungeon parties."
-local defaultPublicWelcomeMsg = "Welcome {p}"
-local defaultOrientationMsg = "So the guild is currently in open recruitment. This means that everyone can recruit and that you should feel free to /ginvite people you meet in your travels who could benifit from guild membership. We try to organize dungeon runs in the evenings and weekends so watch out for those. Also we have a shiny new Discord server, link in the MOTD, which you can check out if you are interested."
-local discordInfo = "Discord Server: https://discord.gg/vMUmvY9Fpy - Please set your nickname to match your character's name."
+local db
+local defaultDB = {
+    recruitMsg = "Hello {p}, would you be interested in joining a leveling guild? We're a light hearted group looking for more people interested in quest and dungeon parties.",
+    recruits = 0,
+    welcomeMsg = "Welcome to the guild, {p}!",
+    orientationMsg = "So the guild is currently in open recruitment. This means that everyone can recruit and that you should feel free to /ginvite people you meet in your travels who could benifit from guild membership. We try to organize dungeon runs in the evenings and weekends so watch out for those. Also we have a shiny new Discord server, link in the MOTD, which you can check out if you are interested.",
+    welcomes = 0,
+    discordInfo = "Discord Server: https://discord.gg/vMUmvY9Fpy - Please set your nickname to match your character's name."
+}
+-- local defaultRecruitmentMsg = "Hello {p}, would you be interested in joining a leveling guild? We're a light hearted group looking for more people interested in quest and dungeon parties."
+-- local defaultPublicWelcomeMsg = "Welcome to the guild, {p}!"
+-- local defaultOrientationMsg = "So the guild is currently in open recruitment. This means that everyone can recruit and that you should feel free to /ginvite people you meet in your travels who could benifit from guild membership. We try to organize dungeon runs in the evenings and weekends so watch out for those. Also we have a shiny new Discord server, link in the MOTD, which you can check out if you are interested."
+-- local discordInfo = "Discord Server: https://discord.gg/vMUmvY9Fpy - Please set your nickname to match your character's name."
 
 function PathfinderTools:getTargetPlayer()
     if UnitName("Target") ~= nil and UnitIsPlayer("Target") then
@@ -22,17 +31,6 @@ function PathfinderTools:getPlayerSelection(name)
     return name
 end
 
-function PathfinderTools:Recruit(name)
-    player = PathfinderTools:getPlayerSelection(name)
-    if player ~= nil then
-        if UnitIsUnit(player,UnitName("player")) then
-            print("PF Error: You can't recruit yourself....")
-        else 
-            SendChatMessage(defaultRecruitmentMsg:gsub("{p}",player), "WHISPER", "Common", player)
-        end
-    end
-end
-
 function PathfinderTools:sendLongMessage(msg,to)
     words = {}
     s = ""
@@ -46,14 +44,27 @@ function PathfinderTools:sendLongMessage(msg,to)
     end
 end
 
+function PathfinderTools:Recruit(name)
+    player = PathfinderTools:getPlayerSelection(name)
+    if player ~= nil then
+        if UnitIsPlayer(player) then
+            print("PF Error: You can't recruit yourself....")
+        else 
+            SendChatMessage(db.recruitMsg:gsub("{p}",player), "WHISPER", "Common", player)
+            db.recruits = db.recruits + 1
+        end
+    end
+end
+
 function PathfinderTools:Welcome(name)
     player = PathfinderTools:getPlayerSelection(name)
     if player ~= nil then
-        if UnitIsUnit(player,"player") then
+        if UnitIsPlayer(player) then
             print("PF Error: You can't welcome yourself....")
         else
-            SendChatMessage(defaultPublicWelcomeMsg:gsub("{p}",player),"GUILD","Common",nil)
-            PathfinderTools:sendLongMessage(defaultOrientationMsg,player)
+            SendChatMessage(db.welcomeMsg:gsub("{p}",player),"GUILD","Common",nil)
+            PathfinderTools:sendLongMessage(db.orientationMsg,player)
+            db.welcomes = db.welcomes + 1
         end
     end
 end
@@ -69,9 +80,88 @@ function PathfinderTools:ShareDiscord(with)
     end
 end
 
+function PathfinderTools:showStats()
+    local rate = 0.0
+    if db.recruits > 0 then
+        rate = ((db.welcomes/db.recruits)*100)
+    end
+    print("--PathfinderTools Stats--")
+    print("Recruit Attempts: "..db.recruits)
+    print("Recruits Welcomed: "..db.welcomes)
+    print("Converstion Rate: "..rate.."%")
+end
+
+function PathfinderTools:getGuildMemberInfo(member)
+    SetGuildRosterShowOffline(true) --show offline members
+    GuildRoster()
+    local totalMembers, onlineMembers, mobileMembers = GetNumGuildMembers()
+    local member = nil
+
+    local count = totalMembers
+
+    while count >= 0 do
+        local m = {
+            name = "",
+            rankName = "",
+            rankIndex = "",
+            level = "",
+            classDisplayName = "",
+            zone = "",
+            publicNote = "",
+            officerNote = "",
+            isOnline = "",
+            status = "",
+            class = "",
+            achievementPoints = "",
+            achievementRank = "",
+            isMobile = "",
+            canSoR = "",
+            repStanding = "",
+            GUID = "",
+            lastOnline = {}
+        }
+        local name, rankName, rankIndex, level, classDisplayName, zone, publicNote, officerNote, isOnline, status, class, achievementPoints, achievementRank, isMobile, canSoR, repStanding, GUID = GetGuildRosterInfo(count)
+        local lastOnline = { GetGuildRosterLastOnline(count) }
+        iwwwwwwwwwwwwwwwwwf name:lower() == member:lower() then
+            member = {
+                name = name,
+                rankName = rankName,
+                rankIndex = rankIndex,
+                level = level,
+                classDisplayName = classDisplayName,
+                zone = zone,
+                publicNote = publicNote,
+                officerNote = officerNote,
+                isOnline = isOnline,
+                status = status,
+                class = class,
+                achievementPoints = achievementPoints,
+                achievementRank = achievementRank,
+                isMobile = isMobile,
+                canSoR = canSoR,
+                repStanding = repStanding,
+                GUID = GUID,
+                lastOnline = lastOnline
+            }
+            break
+        end
+        count = count - 1
+    end
+
+    return member
+end
+
 local function OnEvent(self, event, ...)
     --print(event, ...)
     if event == "ADDON_LOADED" and ... == "PathfinderTools" then
+        PathfinderToolsDB = PathfinderToolsDB or CopyTable(defaultDB)
+        --Add new options from defaultDB
+        for k, v in pairs(defaultDB) do
+			if PathfinderToolsDB[k] == nil then -- don't reset false values
+				PathfinderToolsDB[k] = v
+			end
+        end
+        db = PathfinderToolsDB
         print("PathfinderTools Loaded ...")
     end
 end
@@ -84,7 +174,7 @@ SLASH_PF1 = '/pf'
 function SlashCmdList.PF(args, editbox)
     local _, _, cmd, arg = string.find(args, "%s?(%w+)%s?(.*)")
 
-    print(cmd.." : "..arg)
+    --print(cmd.." : "..arg)
 
     if cmd == 'recruit' then
         PathfinderTools:Recruit(arg)
@@ -92,6 +182,8 @@ function SlashCmdList.PF(args, editbox)
         PathfinderTools:Welcome(arg)
     elseif cmd == "discord" then
         PathfinderTools:ShareDiscord(arg)
+    elseif cmd == 'stats' then
+        PathfinderTools:showStats()
     else
         print("PF Error: Invalid command.")
     end
--- README.md
+++ README.md
@@ -16,7 +16,14 @@
 
     /pf discord [destination] - Sends discord server link to [destination]. Accepted destinations include "raid", "party", "guild" or a player name. If the destination is omitted the current target will be used if it is a player.
 
+### Stats
+
+    /pf stats - dsiplayes recruitment stats
+
 ## To-Do
 
-    * Create options to customize recruitment and welcome text
-    * Add functionality to promote new members to Recruit
+    * Create options to customize recruitment and orientation text 
+    * See if we can combine /ginvite with welcome functionality
+    ** Would modify welcome to include ginvite and check guild log to wait for player joining the guild.
+    ** This would need a timeout for the life of the invite and duplicaiton protection.
+    *** what is in the log? does it show declines?
